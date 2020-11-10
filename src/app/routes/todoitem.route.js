6f7eab67a705c1a9bca7c7c31d0a86decfff8e3b
Ivan_WEB <ivan.exceedteam@gmail.com>
2020-11-10 09:50:50 +0300

    Create routes

--- src/app/routes/todoitem.route.js
+++ src/app/routes/todoitem.route.js
@@ -1,1 +1,11 @@
 //routes for todo
+const express = require('express');
+const router = express.Router();
+
+// Require the controllers WHICH WE DID NOT CREATE YET!!
+const todo_controller = require('../controllers/todoitem.controller');
+
+
+// a simple test url to check that all of our files are communicating correctly.
+router.get('/test', todo_controller.test);
+module.exports = router;
--- src/app/routes/user.route.js
+++ src/app/routes/user.route.js
@@ -1,1 +1,11 @@
 //routes for user
+const express = require('express');
+const router = express.Router();
+
+// Require the controllers WHICH WE DID NOT CREATE YET!!
+const user_controller = require('../controllers/user.controller');
+
+
+// a simple test url to check that all of our files are communicating correctly.
+router.get('/test', user_controller.test);
+module.exports = router;
